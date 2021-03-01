2836bb9be4c473d0dbadfcbe6db8d4304e2958e4
Chyngyz <chyngyz1996@gmail.com>
2021-03-01 17:01:56 -0500

    Initial Commit

---
+++ src/com/chyngyz/todolist/DialogController.java
@@ -0,0 +1,33 @@
+package com.chyngyz.todolist;
+
+import com.chyngyz.todolist.datamodel.TodoData;
+import com.chyngyz.todolist.datamodel.TodoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+
+/**
+ * Created by Chyngyz on 11/17/2020.
+ */
+public class DialogController {
+
+    @FXML
+    private TextField shortDescriptionField;
+    @FXML
+    private TextArea detailsArea;
+    @FXML
+    private DatePicker deadlinePicker;
+
+    public TodoItem processResults() {
+        String shortDescription = shortDescriptionField.getText().trim();
+        String details = detailsArea.getText().trim();
+        LocalDate deadlineValue = deadlinePicker.getValue();
+
+        TodoItem newItem = new TodoItem(shortDescription, details, deadlineValue);
+        TodoData.getInstance().addTodoItem(newItem);
+        return newItem;
+    }
+}
---
+++ src/com/chyngyz/todolist/Main.java
@@ -0,0 +1,43 @@
+package com.chyngyz.todolist;
+
+import com.chyngyz.todolist.datamodel.TodoData;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("mainwindow.fxml"));
+        primaryStage.setTitle("Todo List");
+        primaryStage.setScene(new Scene(root, 900, 500));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        try {
+            TodoData.getInstance().storeTodoItem();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void init() throws Exception {
+        try {
+            TodoData.getInstance().loadTodoItems();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
